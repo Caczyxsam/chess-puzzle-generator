27a7699ce5dddf71dce1c683a7423a731bae7b2d
Caczyxsam <samuel.reinila15@gmail.com>
2026-06-27 12:48:23 +0300

    Added a background to be used, added a logo

--- src/GUI.py
+++ src/GUI.py
@@ -1,6 +1,8 @@
 import chess
 import fetcher
 import random
+import chess.svg
+from IPython.display import display
 
 seed = random.randint(0,1000)
 
@@ -9,5 +11,27 @@ rating = fetcher.puzzles_df.iloc[seed]["Rating"]
 
 board = chess.Board(Fen)
 
-print(board)
-print(f"The puzzle is rated {rating}")
+#print(f"The puzzle is rated {rating}")
+
+board_svg = chess.svg.board(board, size=400)
+
+with open("chessboard.svg", "w") as f:
+    f.write(board_svg)
+
+right_moves = fetcher.puzzles_df.iloc[seed]["Moves"]
+
+#print(right_moves)
+
+#Taustakuva
+
+
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Chess Puzzle Generator")
+icon = tk.PhotoImage(file="src\chess_puzzle_solver_logo.png")
+root.iconphoto(True, icon)
+
+canvas = tk.Canvas(root, width=800, height=800, bg="white")
+canvas.pack()
+root.mainloop()
--- src/fetcher.py
+++ src/fetcher.py
@@ -2,7 +2,3 @@ import pandas as pd
 
 puzzles_csv = ("data\puzzledata.csv")
 puzzles_df = pd.read_csv(puzzles_csv, skipinitialspace=True)
-
-rating = puzzles_df[]
-
-print(puzzles_df.iloc[0]["FEN"])
